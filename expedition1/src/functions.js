80e34fdd1b4c12441bf2f9485a0a292a73741e19
Bella <bella.kamala@gmail.com>
2023-04-04 18:16:22 +0300

    added task 1

--- expedition1/src/functions.js
+++ expedition1/src/functions.js
@@ -1,33 +1,82 @@
+const fs = require("fs");
+
+const crewFile = './src/crew.txt';
+const equipmentFile = './src/equipment.txt';
+const rocketsFile = './src/rockets.txt';
+
+const crew = fs.readFileSync(crewFile, 'utf-8').split('\n').map(line => line.trim())
+const equipment = fs.readFileSync(equipmentFile, 'utf-8').split('\n').map(line => line.trim());
+const rockets = fs.readFileSync(rocketsFile, 'utf-8').split('\n').map(line => line.trim());
+
+console.log(rockets)
 
 
-//позволяет выбрать самого опытного капитана
+
+
+// позволяет выбрать самого опытного капитана
+
+
 function getRightCaptain() {
 
+  const result = crew
+    .slice(1)
+    .map(person => person.split(','))
+    .filter(([fullName, sex, job, experience]) => job === ' Капитан')
+    .sort((a, b) => b[3] - a[3])
+  return result[0].toString()
+
 }
 
-//позволяет выбрать самого опытного врача среди женщин
+
+
+// позволяет выбрать самого опытного врача среди женщин
 function getRightDoc() {
 
+  const result = crew
+    .slice(1)
+    .map(person => person.split(','))
+    .filter(([fullName, sex, job, experience]) => sex === ' ж' && job === ' Врач')
+    .sort((a, b) => b[3] - a[3])
+
+  return result[0].toString()
+
 }
 
-//позволяет выбрать всех бортмехаников
+
+// позволяет выбрать всех бортмехаников
 function getAllEngineer() {
-
+  const result = crew
+    .slice(1)
+    .map(person => person.split(','))
+    .filter(([fullName, sex, job, experience]) => job === ' Бортмеханик')
+  return result.map(person => person.toString())
 }
 
-//Позволяет отобрать все марсоходы
-function getAllRover(){
-
+// Позволяет отобрать все марсоходы
+function getAllRover() {
+  const result = equipment
+    .slice(1)
+    .map(el => el.split(','))
+    .filter(([roverName, type, lifeTime]) => type === ' марсоход')
+  return result.map(el => el.toString())
 }
 
-//позволяет выбрать только те марсоходы, которые смогут прослужить больше 3 лет
-function getRightRovers(){
-
+// позволяет выбрать только те марсоходы, которые смогут прослужить больше 3 лет
+function getRightRovers() {
+  const result = equipment
+    .slice(1)
+    .map(el => el.split(','))
+    .filter(([roverName, type, lifeTime]) => type === ' марсоход' && lifeTime > 3)
+  return result.map(el => el.toString())
 }
 
-//позволяет выбрать ракету с максимальной дальностью полёта
-function getRightRocket(){
-
+// позволяет выбрать ракету с максимальной дальностью полёта
+function getRightRocket() {
+  const result = rockets
+    .slice(1)
+    .map(el => el.split(','))
+    .sort(([rocketNameA, typeA, rangeA], [rocketNameB, typeB, rangeB]) => Number(rangeB) - parseInt(rangeA));
+  return result[0].toString()
 }
 
 
@@ -37,5 +86,5 @@ module.exports = {
   getRightDoc,
   getAllRover,
   getRightRovers,
-  getRightRocket
+  getRightRocket,
 };
